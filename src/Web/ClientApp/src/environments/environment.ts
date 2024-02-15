47e7b98b61ab4260ab7329111ac55f09fdb90ac9
Harsh Patel <harsh3608@gmail.com>
2024-02-15 10:27:12 +0530

    ...

--- src/Web/ClientApp/src/environments/environment.ts
+++ src/Web/ClientApp/src/environments/environment.ts
@@ -12,5 +12,16 @@ export const environment = {
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
+ *
+ *
+ * 
+ * For easier debugging in development mode, yuo can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ *
+ * 
+ * 
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
